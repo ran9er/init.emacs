4966e5c7a69060a129f9088863a7b23c3032bb73
ran9er <2999am@gmail.com>
2011-11-21 14:21:28 +0800

    .

--- 00_func.el
+++ 00_func.el
@@ -153,14 +153,14 @@ See also `define-key-s'."
 
 ;; * pretty symbol
 (defun unicode-symbol (name)
-  "Translate a symbolic name for a Unicode character -- e.g., LEFT-ARROW                                      
+  "Translate a symbolic name for a Unicode character -- e.g., LEFT-ARROW
  or GREATER-THAN into an actual Unicode character code. "
-  (decode-char 'ucs (case name                                             
+  (decode-char 'ucs (case name
                       (left-arrow 8592)
                       (up-arrow 8593)
                       (right-arrow 8594)
-                      (down-arrow 8595)                                                
-                      (double-vertical-bar #X2551)                  
+                      (down-arrow 8595)
+                      (double-vertical-bar #X2551)
                       (equal #X003d)
                       (not-equal #X2260)
                       (identical #X2261)
@@ -168,7 +168,7 @@ See also `define-key-s'."
                       (less-than #X003c)
                       (greater-than #X003e)
                       (less-than-or-equal-to #X2264)
-                      (greater-than-or-equal-to #X2265)                        
+                      (greater-than-or-equal-to #X2265)
                       (logical-and #X2227)
                       (logical-or #X2228)
                       (logical-neg #X00AC)
@@ -182,20 +182,20 @@ See also `define-key-s'."
                       (double-prime #X2033)
                       (for-all #X2200)
                       (there-exists #X2203)
-                      (element-of #X2208)              
+                      (element-of #X2208)
                       (square-root #X221A)
                       (squared #X00B2)
-                      (cubed #X00B3)                                            
+                      (cubed #X00B3)
                       (lambda #X03BB)
                       (alpha #X03B1)
                       (beta #X03B2)
                       (gamma #X03B3)
                       (delta #X03B4))))
 (defun substitute-pattern-with-unicode (pattern symbol)
-  "Add a font lock hook to replace the matched part of PATTERN with the                                       
+  "Add a font lock hook to replace the matched part of PATTERN with the
      Unicode symbol SYMBOL looked up with UNICODE-SYMBOL."
   (font-lock-add-keywords
-   nil `((,pattern 
+   nil `((,pattern
           (0 (progn (compose-region (match-beginning 1) (match-end 1)
                                     ,(unicode-symbol symbol)
                                     'decompose-region)
