b9f32e15ddc3538d83a536db12aa1d2a16fc73e1
ran9er <2999am@gmail.com>
2011-11-30 15:03:29 +0800

    site-start

--- site-start.el
+++ site-start.el
@@ -1,32 +1,43 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (unless (boundp 'init-name);;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(setq   init-name   "init.emacs"
-        init-dir    (expand-file-name
-                     (if (eq system-type 'windows-nt)
-                         (concat exec-directory "../" init-name "/")
-                       (concat "~/" init-name "/"))))
+  (setq init-name "init.emacs")
 
-(if (file-exists-p init-dir)
- (progn
+  (setq init-dir
+        (let ((base-dir
+               (apply 'expand-file-name
+                      (if (eq system-type 'windows-nt)
+                          (list ".." exec-directory)
+                        (list "~" nil)))))
+          (file-name-as-directory
+           (or
+            (car
+             (sort
+              (mapcar (lambda(x) (if (file-directory-p x) x (make-temp-name "")))
+                      (directory-files base-dir t init-name t))
+              'file-newer-than-file-p))
+            (make-temp-name "")))))
 
-   (setq init-time (cons (float-time)()))
+  (if (file-exists-p init-dir)
+      (progn
+        (setq init-time (cons (float-time)()))
 
-   (dolist (init-load-path (cons init-dir (directory-files init-dir t "^_")))
-     (if (file-directory-p init-load-path)
-         (add-to-list 'load-path init-load-path)))
+        (mapc (lambda (init-load-path)
+                (if (file-directory-p init-load-path)
+                    (add-to-list 'load-path init-load-path)))
+              (cons init-dir (directory-files init-dir t "^_")))
 
-   (setq init-files (mapc 'load (directory-files init-dir t "\\.el\\'")))
+        (setq init-files (mapc 'load (directory-files init-dir t "\\.el\\'")))
 
-   (setcdr init-time (float-time))
+        (setcdr init-time (float-time))
 
-   (add-hook 'emacs-startup-hook
-             '(lambda ()
-                (message "load %d init file , spend %g seconds ; startup spend %g seconds"
-                         (length init-files)
-                         (- (cdr init-time) (car init-time))
-                         (- (float-time) (car init-time)))))
-))
+        (add-hook
+         'emacs-startup-hook
+         '(lambda ()
+            (message "load %d init file , spend %g seconds ; startup spend %g seconds"
+                     (length init-files)
+                     (- (cdr init-time) (car init-time))
+                     (- (float-time) (car init-time)))))))
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 );;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
