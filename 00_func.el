72dac5f177f7ae518da59ad553fda3458a18121e
ran9er <2999am@gmail.com>
2012-01-02 22:24:17 +0800

    unicode-symbol

--- 00_func.el
+++ 00_func.el
@@ -50,7 +50,7 @@
 
 (defmacro rqx (action &rest lst)
   "(rqx 0 aaa bbb ccc)"
-;  (list 'rq-x `',action `',lst))
+  ;; (list 'rq-x `',action `',lst))
   `(rq-x ',action ',lst))
 
 ;; * define-key-s
@@ -75,7 +75,7 @@ See also `def-key-s'."
 (defmacro def-k-s (km &rest kd)
   "(def-key-s map \"key\" def \"key\" def ...)
 See also `define-key-s'."
-;  (list 'define-key-s km `',kd))
+  ;; (list 'define-key-s km `',kd))
   `(define-key-s ,km ',kd))
 
 (defun def-key-s (keymap &rest key-defs)
@@ -104,76 +104,71 @@ See also `define-key-s'."
           1 font-lock-warning-face t))))
 
 ;; * pretty symbol
-(defun unicode-symbol (name)
-  "Translate a symbolic name for a Unicode character -- e.g., LEFT-ARROW
- or GREATER-THAN into an actual Unicode character code. "
-  (decode-char 'ucs (case name
-                      (left-arrow 8592)
-                      (up-arrow 8593)
-                      (right-arrow 8594)
-                      (down-arrow 8595)
-                      (double-vertical-bar #X2551)
-                      (equal #X003d)
-                      (not-equal #X2260)
-                      (identical #X2261)
-                      (not-identical #X2262)
-                      (less-than #X003c)
-                      (greater-than #X003e)
-                      (less-than-or-equal-to #X2264)
-                      (greater-than-or-equal-to #X2265)
-                      (logical-and #X2227)
-                      (logical-or #X2228)
-                      (logical-neg #X00AC)
-                      ('nil #X2205)
-                      (dagger #X2020)
-                      (double-dagger #X2021)
-                      (horizontal-ellipsis #X2026)
-                      (reference-mark #X203B)
-                      (double-exclamation #X203C)
-                      (prime #X2032)
-                      (double-prime #X2033)
-                      (for-all #X2200)
-                      (there-exists #X2203)
-                      (element-of #X2208)
-                      (square-root #X221A)
-                      (squared #X00B2)
-                      (cubed #X00B3)
-                      (lambda #X03BB)
-                      (alpha #X03B1)
-                      (beta #X03B2)
-                      (gamma #X03B3)
-                      (delta #X03B4))))
-
-(defun substitute-pattern-with-unicode (pattern symbol)
-  "Add a font lock hook to replace the matched part of PATTERN with the
-     Unicode symbol SYMBOL looked up with UNICODE-SYMBOL."
-  (font-lock-add-keywords
-   nil `((,pattern
-          (0 (progn (compose-region (match-beginning 1) (match-end 1)
-                                    ,(unicode-symbol symbol)
-                                    'decompose-region)
-                    nil))))))
+(defvar *unicode-symbol*
+  (mkht 
+   left-arrow 8592
+   up-arrow 8593
+   right-arrow 8594
+   down-arrow 8595
+   double-vertical-bar #X2551
+   equal #X003d
+   not-equal #X2260
+   identical #X2261
+   not-identical #X2262
+   less-than #X003c
+   greater-than #X003e
+   less-than-or-equal-to #X2264
+   greater-than-or-equal-to #X2265
+   logical-and #X2227
+   logical-or #X2228
+   logical-neg #X00AC
+   'nil #X2205
+   dagger #X2020
+   double-dagger #X2021
+   horizontal-ellipsis #X2026
+   reference-mark #X203B
+   double-exclamation #X203C
+   prime #X2032
+   double-prime #X2033
+   for-all #X2200
+   there-exists #X2203
+   element-of #X2208
+   square-root #X221A
+   squared #X00B2
+   cubed #X00B3
+   lambda #X03BB
+   alpha #X03B1
+   beta #X03B2
+   gamma #X03B3
+   delta #X03B4
+   ))
 
 (defun substitute-patterns-with-unicode (patterns)
-  "Call SUBSTITUTE-PATTERN-WITH-UNICODE repeatedly."
-  (mapcar #'(lambda (x)
-              (substitute-pattern-with-unicode (car x)
-                                               (cdr x)))
-          patterns))
+  ""
+  (mapcar
+   (lambda (x)
+     (font-lock-add-keywords
+      nil `((,(car x)
+             (0 (progn
+                  (compose-region (match-beginning 1) (match-end 1)
+                                  ,(decode-char 'ucs (gethash (cdr x) *unicode-symbol*))
+                                  'decompose-region)
+                  nil))))))
+   patterns))
 
 ;; ** lisp symbol
 (defun lisp-symbol ()
   (interactive)
   (substitute-patterns-with-unicode
    (alist '("(?\\(lambda\\>\\)" lambda
-                ;; "\\<\\(lambda\\)\\>" lambda
-                "\\(;;\\ \\)" reference-mark
-                ;; "\\(<-\\)" left-arrow
-                ;; "\\(->\\)" right-arrow
-                ;; "\\(==\\)" identical
-                ;; "\\(/=\\)" not-identical
-                "\\(>=\\)" greater-than-or-equal-to
-                "\\(<=\\)" less-than-or-equal-to
-                ;; "\\(\\.\\.\\)" horizontal-ellipsis
-                ))))
+            ;; "\\<\\(lambda\\)\\>" lambda
+            "\\(;;\\ \\)" reference-mark
+            ;; "\\(<-\\)" left-arrow
+            ;; "\\(->\\)" right-arrow
+            ;; "\\(==\\)" identical
+            ;; "\\(/=\\)" not-identical
+            "\\(>=\\)" greater-than-or-equal-to
+            "\\(<=\\)" less-than-or-equal-to
+            ;; "\\(\\.\\.\\)" horizontal-ellipsis
+            ))))
 
