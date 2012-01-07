13e569ac588d852d5d8e99f900232a8d660e0eda
ran9er <2999am@gmail.com>
2012-01-07 21:17:29 +0800

    .

--- 11_addons.el
+++ 11_addons.el
@@ -1,7 +1,7 @@
 ;; -*- encoding: utf-8-unix; -*-
-;; *========== ibuffer		
+;; *========== ibuffer
 (require 'ibuffer)
-;(global-set-key ( kbd "C-x C-b ")' ibuffer)
+;; (global-set-key ( kbd "C-x C-b ")' ibuffer)
 
 ;; *========== undo-tree
 (require 'undo-tree)
@@ -12,28 +12,24 @@
 (require 'anything)
 
 ;; *========== browse-kill-ring
-(require 'browse-kill-ring) 
-(global-set-key [(control c)(k)] 'browse-kill-ring) 
-(browse-kill-ring-default-keybindings) 
+(require 'browse-kill-ring)
+(global-set-key [(control c)(k)] 'browse-kill-ring)
+(browse-kill-ring-default-keybindings)
 
 ;; *========== minibuffer-complete-cycle
 (require 'minibuffer-complete-cycle)
 (setq minibuffer-complete-cycle t)
 
 ;; *========== multi-term
-;	(require 'multi-term)
-;	
-;	(if (eq system-type 'gnu/linux)
-;	  (setq multi-term-program "/bin/bash")
-;	)
-
+;; (require 'multi-term)
+;; (if (eq system-type 'gnu/linux)
+;;     (setq multi-term-program "/bin/bash"))
 
 ;; *========== docbook
-;(require 'docbook-xml-mode)
-
-;(add-hook 'docbook-xml-mode-hook
-;	  (function (lambda ()
-;                  (setq outline-regexp "<!\\-\\-\\*+")
-;		      (outline-minor-mode)
-;		      (hide-body)
-;	    		)))
+;; (require 'docbook-xml-mode)
+;; (add-hook 'docbook-xml-mode-hook
+;;           (function
+;;            (lambda ()
+;;              (setq outline-regexp "<!\\-\\-\\*+")
+;;              (outline-minor-mode)
+;;              (hide-body))))
