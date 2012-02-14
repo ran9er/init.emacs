1f6fd2ef1d90e13f19a797c6c3ce38a6b0509228
ran9er <2999am@gmail.com>
2012-02-15 01:01:00 +0800

    .

--- _autoload_/+script.el
+++ _autoload_/+script.el
@@ -1,7 +1,7 @@
 ;; -*- encoding: utf-8-unix; -*-
 ;; File-name:    <+script.el>
 ;; Create:       <2012-02-14 00:21:56 ran9er>
-;; Time-stamp:   <2012-02-14 01:31:36 ran9er>
+;; Time-stamp:   <2012-02-15 00:57:56 ran9er>
 ;; Mail:         <2999am@gmail.com>
 
 ;;;###autoload
@@ -42,12 +42,19 @@ File suffix is used to determine what program to run."
     (and status (message "%s" status))))
 
 ;;;###autoload
-(defmacro x-shell (prog)
-  `(defun ,(concat-symbol prog '-shell)()
-     ,(format "make a %s shell" prog)
-     (interactive)
-     (switch-to-buffer (make-comint ,(symbol-name prog) ,(symbol-name prog) nil "-i"))))
+(defun x-shell (prog)
+  (eval
+   `(defun ,(concat-symbol prog '-shell)()
+      ,(format "make a %s shell" prog)
+      (interactive)
+      (switch-to-buffer (make-comint ,(symbol-name prog) ,(symbol-name prog) nil "-i")))))
+;; (mapcar 'x-shell '(python lua))
 
+;; (defmacro x-shell (prog)
+;;   `(defun ,(concat-symbol prog '-shell)()
+;;      ,(format "make a %s shell" prog)
+;;      (interactive)
+;;      (switch-to-buffer (make-comint ,(symbol-name prog) ,(symbol-name prog) nil "-i"))))
 ;; (mapcar (lambda(x)(eval `(x-shell ,x))) '(python lua))
 
 ;;;###autoload
