5e78e26c295dd7691c3090064ed4ee09601ab965
ran9er <2999am@gmail.com>
2012-04-11 00:04:18 +0800

    .

--- _autoload_/+arith.el
+++ _autoload_/+arith.el
@@ -1,10 +1,9 @@
 ;; -*- encoding: utf-8-unix; -*-
 ;; File-name:    <+arith.el>
 ;; Create:       <2012-04-10 23:05:51 ran9er>
-;; Time-stamp:   <2012-04-10 23:23:21 ran9er>
+;; Time-stamp:   <2012-04-11 00:02:40 ran9er>
 ;; Mail:         <2999am@gmail.com>
 
 ;;;###autoload
 (defun average (&rest lst)
-  (apply (lambda(&rest x)(/ (float (apply '+ x)) (length x)))
-         lst))
+  (/ (float (apply '+ lst)) (length lst)))
