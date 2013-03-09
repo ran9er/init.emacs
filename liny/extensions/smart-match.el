3356e91edd5bb8c085408a1850c3459d4e9cb05d
ran9er <ignitron@gmail.com>
2013-03-09 11:05:04 +0800

    liny: fixed smart-match

--- liny/extensions/smart-match.el
+++ liny/extensions/smart-match.el
@@ -1,52 +1,49 @@
 ;; * match
 (setq liny-match-strategy 'liny-smart-match)
 
-(defvar liny-env-test
-  `((,major-mode  major-mode t)
-    (head (progn (skip-chars-backward " \t\n")(bobp)))
+(defvar liny-env-test-necessary
+  '())
+
+(defvar liny-env-test-sufficient
+  '((head (progn (skip-chars-backward " \t\n")(bobp)))
     (tail (progn (skip-chars-forward " \t\n")(eobp)))
     (notop (null (zerop (current-indentation))))
-    (top (null (zerop (current-indentation))))
-    ))
+    (top (null (zerop (current-indentation))))))
 
-(defun liny-fetch-env ()
+(defun liny-fetch-env-sufficient ()
   "liny-fetch-env "
   (let ((test
-         (lambda(s)
+         (lambda(tst)
            (sort
             (remove
              nil
              (mapcar
               (lambda(x)
-                (if (and
-                     (eq (nth 2 x) s)
-                     (save-excursion (save-restriction (eval (nth 1 x)))))
-                    (symbol-name (nth 0 x))))
-              liny-env-test))
+                (if (save-excursion (save-restriction (eval (nth 1 x))))
+                    (cons (symbol-name (nth 0 x)) (or (nth 2 x) 1))))
+              tst))
             (lambda(x y)
               (string-lessp x y))))))
-    (list (funcall test t)(funcall test nil))))
+        (funcall test liny-env-test-sufficient)))
 
-(defun liny-keywords-match (modes keywords)
+(defun liny-fetch-env-mode ()
+  "liny-fetch-env-mode is writen by ran9er"
+  major-mode)
+
+(defun liny-keywords-match (&optional modes keywords)
   "liny-keywords-match is writen by ran9er"
   (let* (test
-         (env (liny-fetch-env))
-         (test1 (nth 0 env))
-         (test2 (nth 1 env))
+         (env (liny-fetch-env-sufficient))
          (result 0))
-    (if (member "all" modes)
-        (setq test t)
-      (while
-          (and test1
-               (null (if (member (car test1) modes)
-                   (setq test t))))
-        (setq test1 (cdr test1))))
+    (if (or (member "all" modes)
+            (member (symbol-name (liny-fetch-env-mode)) modes))
+        (setq test t))
     (if test
         (mapc
          (lambda(x)
-           (if (member x keywords)
-               (setq result (1+ result))))
-         test2))
+           (if (member (car x) keywords)
+               (setq result (+ (cdr x) result))))
+         env))
     (and test result)))
 
 ;; * index
@@ -106,8 +103,9 @@
        (lambda(x)
          (let* ((lst (cdr (assoc x (cdr liny-index))))
                 (modes (nth 0 lst))
-                (keywords (nth 1 lst)))
-           (cons (liny-keywords-match modes keywords) x)))
+                (keywords (nth 1 lst))
+                (match (liny-keywords-match modes keywords)))
+           (if match (cons match x))))
        files)
       (lambda(x y)
         (> (car x)(car y)))))))
