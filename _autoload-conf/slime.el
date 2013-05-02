40be3c26c88f2ed6eecc3041aa36dbfb78cc125a
ForeDawn <ignitron@gmail.com>
2013-05-03 00:33:56 +0800

    .

--- _autoload-conf/slime.el
+++ _autoload-conf/slime.el
@@ -1,23 +1,31 @@
 (add-to-list 'load-path (expand-file-name "slime" exts-dir))
-;; (setenv "PATH" (concat
-;;                 (expand-file-name "../../sbcl" exec-directory)
-;;                 ";" (getenv "PATH")))
-(setenv "SBCL_Home" (expand-file-name "../../sbcl" exec-directory))
+(require 'slime-autoloads)
+
+(if (eq system-type 'windows-nt)
+    (progn
+      (setq slime-lisp-implementations
+            `((ccl (,(expand-file-name "../../ccl/wx86cl.exe" exec-directory))
+                   :coding-system utf-8-unix)
+              (sbcl (,(expand-file-name "../../sbcl/sbcl.exe" exec-directory))
+                    :coding-system utf-8-unix)
+              (Racket (,(expand-file-name "../../Racket/Racket.exe" exec-directory))
+                      :coding-system utf-8-unix)
+              (cmucl ("cmucl" "-quiet"))))
+      (setenv "SBCL_Home" (expand-file-name "../../sbcl" exec-directory))))
+
 ;; (setq inferior-lisp-program
 ;;       (expand-file-name "../../sbcl/sbcl.exe" exec-directory))
-(setq slime-lisp-implementations
-      `((sbcl (,(expand-file-name "../../sbcl/sbcl.exe" exec-directory))
-              :coding-system utf-8-unix)
-        (ccl (,(expand-file-name "../../ccl/wx86cl.exe" exec-directory))
-             :coding-system utf-8-unix)
-        (Racket (,(expand-file-name "../../Racket/Racket.exe" exec-directory))
-                :coding-system utf-8-unix)
-        (cmucl ("cmucl" "-quiet"))))
-(require 'slime-autoloads)
+
 (global-set-key "\C-cs" 'slime-selector)
 (setq inferior-lisp-program "lisp")
-(slime-setup '(slime-fancy slime-asdf slime-banner))
+(slime-setup '(slime-fancy
+               slime-asdf
+               slime-banner
+               slime-clipboard
+               slime-scheme
+               ))
 (setq slime-complete-symbol*-fancy t)
 (setq slime-complete-symbol-function 'slime-fuzzy-complete-symbol)
+
 ;;;###autoload
 (autoload 'slime "slime-autoloads" "" t)
