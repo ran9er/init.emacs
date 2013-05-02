03d800118073cec728b4c44bc0307326e2fd2c83
ran9er <ignitron@gmail.com>
2013-05-02 17:51:00 +0800

    + scheme-mode

---
+++ _autoload-conf/scheme.el
@@ -0,0 +1,18 @@
+(setenv "PATH" (concat
+                "D:/apps/Racket"
+                ";" (getenv "PATH")))
+
+(setq scheme-program-name (if (eq system-type 'windows-nt)
+                                "Racket" "guile"))
+
+(setq quack-default-program scheme-program-name)
+
+;; (setq quack-fontify-style nil)
+
+;;;###autoload
+(autoload 'run-scheme (expand-file-name "quack" exts-dir)
+  "Quack scheme editing mode" t)
+
+;;;###autoload
+(autoload 'scheme-mode (expand-file-name "quack" exts-dir)
+  "Quack scheme editing mode" t)
